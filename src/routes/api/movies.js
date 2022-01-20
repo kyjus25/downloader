306b795b1f3c0b41462e4e480e16e618fc7ea861
Justin White <kyjus25@gmail.com>
2022-01-19 21:24:32 -0600

    Updates

--- src/routes/api/movies.js
+++ src/routes/api/movies.js
@@ -5,6 +5,7 @@
 
 import * as provider1 from '$lib/providers/123movie.lc.js';
 import * as provider2 from '$lib/providers/123moviess.se.js';
+import * as provider3 from '$lib/providers/supernova.to.js';
 
 const download = async (name, url) => {
     https.get(url, res => {
@@ -23,16 +24,16 @@ const download = async (name, url) => {
 export const get = async (request) => {
     const title = request.url.searchParams.get('title');
     const providers = [
-        provider1, provider2
+        provider1, provider2, provider3
     ];
 
-    const results = await Promise.all(providers.map(i => i.search(title)));
-    // const res = await provider2.search(title);
+    //const results = await Promise.all(providers.map(i => i.search(title)));
+    const res = await provider3.search(title);
 
 
     // console.log(movies)
     return {
-        body: results
+        body: res
     };
 }
 
